0c7b07bf8d16b6f54163a1b69ba52416f969c04e
Lucas Morales <lucasvmorales@protonmail.com>
2022-03-28 18:22:03 -0300

    Cadastro de usuários com Laravel

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Redirect;
 use App\Models\Usuario;
 use Illuminate\Http\Request;
 
@@ -10,7 +11,17 @@ class UsuariosController extends Controller
     public function index()
     {
         $usuarios = Usuario::get();
-
         return view('usuarios.list', ['usuarios' => $usuarios]);
+    }
+
+    public function new()
+    {
+        return view('usuarios.form');
+    }
+    public function add(Request $request)
+    {
+        $usuario = new Usuario();
+        $usuario = $usuario->create($request->all());
+        return Redirect::to('/usuarios');
     }
 }
--- resources/views/usuarios/list.blade.php
+++ resources/views/usuarios/list.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header"><a href="{{url('usuarios/new')}}">Novo usuário</a></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,7 +18,7 @@
                     @foreach ($usuarios as $u)
                     <p>{{ $u->name }} | {{$u->email}}</p>
                     @endforeach
-                    
+
                 </div>
             </div>
         </div>
